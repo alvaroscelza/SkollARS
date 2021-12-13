08a309cccb6dbbf0facd0841b76a3778d0598b58
Loaderon <alvaroscelza@gmail.com>
2021-12-13 17:27:42 -0300

    added footer

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div className="text-center">
+            Copyright © {(new Date().getFullYear())} SkollARS
+        </div>
+    );
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './assets/theme.scss';
 import Header from './Header';
 import Home from './Home';
+import Footer from './Footer';
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
     <Home />
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
