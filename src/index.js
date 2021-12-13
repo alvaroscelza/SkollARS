0b3e08db47755565f8534c2665234b86e1ed21aa
Loaderon <alvaroscelza@gmail.com>
2021-12-12 23:16:36 -0300

    Home page first draft

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './Header';
 import './assets/custom.css';
 import './assets/theme.scss';
+import Header from './Header';
+import Home from './Home';
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
