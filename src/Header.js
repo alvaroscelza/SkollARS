557f38bcac123c5b941a625441309e9757c5bc5a
Loaderon <alvaroscelza@gmail.com>
2021-12-12 22:29:32 -0300

    changes on header

---
+++ src/Header.js
@@ -0,0 +1,21 @@
+const Header = () => {
+    return (
+        <nav className="navbar navbar--primary navbar-expand-lg navbar-light bg-white">
+            <div className="container">
+                <a className="navbar-brand" href="/">
+                    <img className="logo header-logo-dark" alt="SkollARS" src="/named_logo.png" />
+                </a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target=".navMenu" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div id="main-nav" className="collapse navbar-collapse justify-content-end navMenu">
+                    <ul className="navbar-nav">
+                        <li className="nav-item"><a className="nav-link h5" href="/">Home</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './Header'
+import Header from './Header';
+import './assets/custom.css';
+import './assets/theme.scss';
 
 ReactDOM.render(
   <React.StrictMode>
