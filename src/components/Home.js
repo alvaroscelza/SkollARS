8930cbdfe41bc9fbdf97646da1a70607c6b33c1f
Loaderon <alvaroscelza@gmail.com>
2022-01-01 17:13:00 -0300

    Using component for Products

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import { Fragment } from "react/cjs/react.production.min";
 import ContactForm from "./ContactForm";
+import Product from "./Product";
 
 const Header = () => {
   return (
@@ -23,100 +24,90 @@ const Header = () => {
           </div>
         </div>
       </section>
-
       <section className="bg-white pt-0">
         <div className="container">
           <div className="row justify-content-center">
-            <div className="col-12 col-md-6 col-lg-4 mb-4">
-              <div className="text-center text-md-left">
-                <img src="/one_gear.png" alt="" className="icon flex-shrink-0 mb-1" />
-                <div>
-                  <h5 className="h4 mb-2 mt-2 font-weight-bold text-center text-md-left">Soluciones Estándar</h5>
-                  <p className="text-center text-md-left pr-md-2">
-                    Software funcional, listo para usarse. Soporta pequeñas customizaciones. <br />
-                    <br />
-                    Principalmente software de gestión: RRHH, Finanzas, Inventario, Punto de Venta, entre otros.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col-12 col-md-6 col-lg-4 mb-4">
-              <div className="text-center text-md-left">
-                <img src="/two_gears.png" alt="" className="icon flex-shrink-0 mb-1" />
-                <div>
-                  <h5 className="h4 mb-2 mt-2 font-weight-bold text-center text-md-left">Soluciones Prediseñadas</h5>
-                  <p className="text-center text-md-left pr-md-2">
-                    Software semi funcional para modelos de negocios estándar. Soporta customizaciones medianas. <br />
-                    <br />
-                    Software que suele requerir ciertas customizaciones para quedar funcional: e-commerce, blogs,
-                    landing pages.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col-12 col-md-6 col-lg-4 mb-4">
-              <div className="text-center text-md-left">
-                <img src="/three_gears.png" alt="" className="icon flex-shrink-0 mb-1" />
-                <div>
-                  <h5 className="h4 mb-2 mt-2 font-weight-bold text-center text-md-left">Desarrollo A Medida</h5>
-                  <p className="text-center text-md-left pr-md-2">
-                    Desarrollo de Software completamente a medida. No importa lo que necesites, lo construiremos para
-                    ti, con las funcionalidades que tu quieres y de la forma que tu quieres.
-                    <br />
-                    <br />
-                    Incluye ingeniería de requerimientos, donde te ayudaremos a explorar las mejores ideas para que tu
-                    nuevo software potencie tu negocio lo máximo posible.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col-12 col-md-6 col-lg-4">
-              <div className="text-center text-md-left">
-                <img src="/design_thinking.png" alt="" className="icon icon-lg flex-shrink-0 mb-1" />
-                <div>
-                  <h5 className="h4 mb-2 mt-2 font-weight-bold text-center text-md-left">Design Thinking</h5>
-                  <p className="text-center text-md-left pr-md-2">
-                    Te ayudamos a entender y definir los problemas de tu negocio. Y luego a pensar y validar soluciones
-                    para estos. <br />
-                    <br />
-                    Design Thinking es una metodología de diseño de soluciones, permite explorar ideas para poder
-                    validarlas y/o mejorarlas con bajo costo y de forma temprana.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col-12 col-md-6 col-lg-4">
-              <div className="text-center text-md-left">
-                <img src="/finance_engineering.png" alt="" className="icon icon-lg flex-shrink-0 mb-1" />
-                <div>
-                  <h5 className="h4 mb-2 mt-2 font-weight-bold text-center text-md-left">Optimización De Negocios</h5>
-                  <p className="text-center text-md-left pr-md-2">
-                    Analizamos tu negocio en busca de mejoras tecnológicas de las que puedas beneficiarte. El análisis
-                    es superficial y está orientado a detectar mejoras sustanciales de bajo costo y alto impacto.
-                    <br />
-                    <br />
-                    Es ideal para pequeños negocios o emprendimientos personales.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col-12 col-md-6 col-lg-4">
-              <div className="text-center text-md-left">
-                <img src="/information_techs.png" alt="" className="icon icon-lg flex-shrink-0 mb-1" />
-                <div>
-                  <h5 className="h4 mb-2 mt-2 font-weight-bold text-center text-md-left">Sistemas De Información</h5>
-                  <p className="text-center text-md-left pr-md-2">
-                    Análisis exhaustivo de la Unidad Estratégica de tu Negocio (UEN). Este es un estudio mucho más
-                    profundo que la Optimización de Negocio. Se busca una mejora sustancial de la estrategia de negocio
-                    de tu empresa, analizando: sistema de creencias, estrategia genérica, análisis de fuerzas de Porter,
-                    análisis de la Cadena de Valor, posición en TI/SI, análisis FODA, análisis ITSGA, identificación de
-                    ITSAs <br />
-                    <br />
-                    Este producto está pensado para medianos o grandes negocios.
-                  </p>
-                </div>
-              </div>
-            </div>
+            <Product
+              image_file_name="/one_gear.png"
+              title="Soluciones Estándar"
+              text={
+                <Fragment>
+                  Software funcional, listo para usarse. Soporta pequeñas customizaciones. <br />
+                  <br />
+                  Principalmente software de gestión: RRHH, Finanzas, Inventario, Punto de Venta, entre otros.
+                </Fragment>
+              }
+            />
+            <Product
+              image_file_name="/two_gears.png"
+              title="Soluciones Prediseñadas"
+              text={
+                <Fragment>
+                  Software semi funcional para modelos de negocios estándar. Soporta customizaciones medianas. <br />
+                  <br />
+                  Software que suele requerir ciertas customizaciones para quedar funcional: e-commerce, blogs, landing
+                  pages.
+                </Fragment>
+              }
+            />
+            <Product
+              image_file_name="/three_gears.png"
+              title="Desarrollo A Medida"
+              text={
+                <Fragment>
+                  Desarrollo de Software completamente a medida. No importa lo que necesites, lo construiremos para ti,
+                  con las funcionalidades que tu quieres y de laimport Product from './Product'; forma que tu quieres.{" "}
+                  <br />
+                  <br />
+                  Incluye ingeniería de requerimientos, donde te ayudaremos a explorar las mejores ideas para que tu
+                  nuevo software potencie tu negocio lo máximo posible.
+                </Fragment>
+              }
+            />
+            <Product
+              image_file_name="/design_thinking.png"
+              title="Design Thinking"
+              text={
+                <Fragment>
+                  Te ayudamos a entender y definir los problemas de tu negocio. Y luego a pensar y validar soluciones
+                  para estos. <br />
+                  <br />
+                  Design Thinking es una metodología de diseño de soluciones, permite explorar ideas para poder
+                  validarlas y/o mejorarlas con bajo costo y de forma temprana.
+                </Fragment>
+              }
+              icon_size="large"
+            />
+            <Product
+              image_file_name="/finance_engineering.png"
+              title="Optimización De Negocios"
+              text={
+                <Fragment>
+                  Analizamos tu negocio en busca de mejoras tecnológicas de las que puedas beneficiarte. El análisis es
+                  superficial y está orientado a detectar mejoras sustanciales de bajo costo y alto impacto.
+                  <br />
+                  <br />
+                  Es ideal para pequeños negocios o emprendimientos personales.
+                </Fragment>
+              }
+              icon_size="large"
+            />
+            <Product
+              image_file_name="/information_techs.png"
+              title="Sistemas De Información"
+              text={
+                <Fragment>
+                  Análisis exhaustivo de la Unidad Estratégica de tu Negocio (UEN). Este es un estudio mucho más
+                  profundo que la Optimización de Negocio. Se busca una mejora sustancial de la estrategia de negocio de
+                  tu empresa, analizando: sistema de creencias, estrategia genérica, análisis de fuerzas de Porter,
+                  análisis de la Cadena de Valor, posición en TI/SI, análisis FODA, análisis ITSGA, identificación de
+                  ITSAs <br />
+                  <br />
+                  Este producto está pensado para medianos o grandes negocios.
+                </Fragment>
+              }
+              icon_size="large"
+            />
           </div>
         </div>
       </section>
