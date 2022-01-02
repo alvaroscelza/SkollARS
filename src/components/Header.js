0294a42329d811a8e5cd80a03b02c9a6a7ea9f50
Loaderon <alvaroscelza@gmail.com>
2022-01-01 21:29:58 -0300

    links now point to Homepage setting in package.json

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,17 @@
 import named_logo from "../images/named_logo.png";
+const package_json_data = require("../../package.json");
 
 const Header = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light">
       <div className="container">
-        <a className="navbar-brand" href="/">
+        <a className="navbar-brand" href={package_json_data.homepage}>
           <img className="logo header-logo-dark" alt="SkollARS" src={named_logo} />
         </a>
         <div id="main-nav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="h5" href="/">
+              <a className="h5" href={package_json_data.homepage}>
                 Home
               </a>
             </li>
