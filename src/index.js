1d7db248580973014219e604fb6c874729547138
Loaderon <alvaroscelza@gmail.com>
2022-01-09 12:35:59 -0300

    changed custom.scss file name to index.scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./custom.scss";
+import "./index.scss";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
